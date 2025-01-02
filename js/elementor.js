0d43c3336b9f7e1316e019a95a39cc318b5a5407
naveedkhan888 <88194719+naveedkhan888@users.noreply.github.com>
2025-01-02 16:49:10 +0500

    sdfsdf

--- js/elementor.js
+++ js/elementor.js
@@ -563,6 +563,49 @@
         });
     };
 
+
+    var imageboxCarousel = function ($scope, $) {
+        $scope.find('.image-box-carousel_new').each( function () {
+            var $this     = $(this),
+                $loop     = $this.data('loop'),
+                $auto     = $this.data('auto'),
+                $time     = $this.data('time'),
+                $dots     = $this.data('dots'),
+                $nav      = $this.data('arrows'),
+                $items    = $this.data('show') ? $this.data('show') : 3,
+                $titems   = $this.data('tshow') ? $this.data('tshow') : 2,
+                $mitems   = $this.data('mshow') ? $this.data('mshow') : 1,
+                $gaps     = $this.data('gaps') !== '' ? $this.data('gaps') : 30,
+                $tgaps    = $this.data('tgaps') !== '' ? $this.data('tgaps') : 30,
+                $mgaps    = $this.data('mgaps') !== '' ? $this.data('mgaps') : 30,
+                $selector = $this.find('.owl-carousel');
+            $selector.owlCarousel({
+                rtl: rtl_owl(),
+                autoplay:$auto,
+                autoplayTimeout: $time,
+                loop:$loop,
+                responsiveClass:true,
+                dots:$dots,
+                nav:$nav,
+                navText:['<i class="xp-webicon-left-arrow-2"></i>', '<i class="xp-webicon-trajectory"></i>'],
+                responsive : {
+                    0 : {
+                        margin:$mgaps,
+                        items:$mitems,
+                    },
+                    768 : {
+                        margin:$tgaps,
+                        items:$titems,
+                    },
+                    1024 : {
+                        margin:$gaps,
+                        items:$items,
+                    }
+                }
+            });
+        });
+    };
+
     /* --------------------------------------------------
     * social team
     * --------------------------------------------------*/
